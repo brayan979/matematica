d2940f14b9ed093b2b42685ae7787d61a224d29d
Brayan Guarachi <b.guarachi@alumnos.upm.es>
2022-03-08 07:57:54 +0100

    Subir web

---
+++ js/factores_primos.js
@@ -0,0 +1,41 @@
+export const descomposicion = (n) => {
+    let factorPrimo = 2
+    let contarPrimerFactor = true
+    let ls = []
+    while (n>1) {
+        if(n% factorPrimo === 0){
+            if (contarPrimerFactor) {
+                ls.push(factorPrimo)
+                contarPrimerFactor = false
+            }
+            else
+                ls.push(factorPrimo)
+                n /= factorPrimo
+        }
+        else{
+            // Buscar el sigueinte primo
+            factorPrimo++
+            while (!esPrimo(factorPrimo)) {
+                factorPrimo++
+            }
+        }
+            
+    }
+    console.log(ls);
+}
+
+// Verificar si es primo
+export const esPrimo = (n) => {
+    if(n <= 1){
+        return false
+    }
+    let noEsPrimo = false
+    let i = 2
+    while (i <= Math.sqrt(n) && !noEsPrimo) {
+        if (n%i == 0) {
+            noEsPrimo = true
+        }
+        i++
+    }
+    return !noEsPrimo
+}
---
+++ js/mcm_mcd.js
@@ -0,0 +1,34 @@
+const maximo = (n1,n2) => {
+    if (n1 > n2) return n1
+    return n2
+}
+const minimo = (n1,n2) => {
+    if (n1 < n2) return n1
+    return n2
+}
+
+export const division = (num1,num2) => {
+    // mcm 200, mcd 2
+    let resto = 1n
+    let cociente = 0n
+    let salida = false
+
+    let max = maximo(num1,num2)
+    let min = minimo(num1,num2)
+
+    let mcd = 0n
+    while (!salida) {
+        resto = max % min
+        cociente = (max-resto)/ min
+        if (resto == 0) {
+            mcd = min
+            salida = true
+        }else{
+            max = min
+            min = resto
+        }
+    }
+    let mcm = BigInt((num1 * num2)/ mcd)
+    console.log('MCD: ', mcd ,' - MCM: ',mcm ,'')
+}
+
