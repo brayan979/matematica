271b468df501645f21ebf966fd94306b8c14140b
Brayan Guarachi <b.guarachi@alumnos.upm.es>
2022-03-11 11:32:24 +0100

    Presntacion Terminada

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ let btnEratostenes = document.querySelector('.eratostenes')
 let divCriba = document.querySelector('.criba')
 let inputCriba = document.querySelector('.inputCriba')
 
+let btnDescomponer = document.querySelector('.btnDescomponer')
+let contenedro__descomponer = document.querySelector('.contenedro__descomponer')
+let inputDescomponer = document.querySelector('.inputDescomponer')
+
+let inputNum2 = document.querySelector('.inputNum2')
+let inputNum1 = document.querySelector('.inputNum1')
+let contenedorMcmMcd = document.querySelector('.contenedorMcmMcd')
+let btnMcmMcd = document.querySelector('.btnMcmMcd')
 
 btnTartaglia.addEventListener('click', (e) => {
     let triangulo = filas(parseInt(inputTartaglia.value, 10) )
@@ -22,5 +30,15 @@ btnEratostenes.addEventListener('click',() => {
     inputCriba.value = ''
 })
 
-//division(BigInt(10807),BigInt(6876867))
-descomposicion(16000040)
+btnDescomponer.addEventListener('click',() => {
+    contenedro__descomponer.innerText = descomposicion(parseInt(inputDescomponer.value, 10))
+    inputCriba.value = ''
+})
+
+btnMcmMcd.addEventListener('click',() => {
+    let resultado = division(BigInt(inputNum1.value),BigInt(inputNum2.value))
+    contenedorMcmMcd.innerText = 'MCM: '+ resultado[0] + ' - MCD: '+resultado[1]
+    inputNum2.value = ''
+    inputNum1.value = ''
+})
+
