40741d6b4b602198c32908e37652bd269d54ecb9
Brayan Guarachi <b.guarachi@alumnos.upm.es>
2022-03-13 19:42:24 +0100

    Comentarios finales

--- js/Ejercicio_7.js
+++ js/Ejercicio_7.js
@@ -1,3 +1,5 @@
+
+// Indentica el numero maximo y el minimo para proceder con las divisiones
 const maximo = (n1,n2) => {
     if (n1 > n2) return n1
     return n2
@@ -6,8 +8,7 @@ const minimo = (n1,n2) => {
     if (n1 < n2) return n1
     return n2
 }
-
-export const division = (num1,num2) => {
+export const euclides = (num1,num2) => {
     // mcm 200, mcd 2
     let resto = 1n
     let cociente = 0n
@@ -17,6 +18,7 @@ export const division = (num1,num2) => {
     let min = minimo(num1,num2)
 
     let mcd = 0n
+    // Realiza el algoritmo de euclides
     while (!salida) {
         resto = max % min
         cociente = (max-resto)/ min
@@ -28,7 +30,9 @@ export const division = (num1,num2) => {
             min = resto
         }
     }
+    // Proposicion 4.2 : Calculo le minimo comun multiplo con el MCD
     let mcm = BigInt((num1 * num2)/ mcd)
+
     return [mcm,mcd]
 }
 
